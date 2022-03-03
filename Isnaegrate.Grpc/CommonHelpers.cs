650ae9dcd4b591a5d64fe78a09e473d9b5f10f17
nuchs <sjbrown@live.co.uk>
2022-03-03 17:39:24 +0000

    Moved proto's out into nuget package

---
+++ Isnaegrate.Grpc/CommonHelpers.cs
@@ -0,0 +1,20 @@
+﻿using Google.Protobuf;
+using Google.Protobuf.WellKnownTypes;
+using Isnaegrate.Grpc.Events;
+
+namespace Isnaegrate.Grpc;
+
+public static class CommonHelpers
+{
+    public static IsgEvent NewIsgEvent(string source, EventType type, byte[]? payload = null)
+    {
+        return new IsgEvent()
+        {
+            Id = new Uid() { Value = Guid.NewGuid().ToString() },
+            Type = type,
+            Source = source,
+            When = Timestamp.FromDateTime(DateTime.UtcNow),
+            Payload = payload is null ? ByteString.Empty : ByteString.CopyFrom(payload)
+        };
+    }
+}
