a70ef23569251de4a5f18bc38628c4ee7c7da890
nuchs <sjbrown@live.co.uk>
2022-03-07 12:18:58 +0000

    Publishers need to set the id to allow for idempotent publishing

--- Grpcontract/EpoxyHelpers.cs
+++ Grpcontract/EpoxyHelpers.cs
@@ -1,5 +1,7 @@
-﻿using Epoxy.Grpc.Streams;
+﻿using Epoxy.Grpc.Reader;
+using Epoxy.Grpc.Streams;
 using Epoxy.Grpc.Types;
+using Epoxy.Grpc.Shared;
 using Google.Protobuf;
 using Google.Protobuf.WellKnownTypes;
 using System.Text;
@@ -8,17 +10,17 @@ namespace Epoxy.Grpc;
 
 public static class EpoxyHelpers
 {
-    public static IsgEvent NewIsgEvent(string id, string stream, string type, string source, ulong position, DateTime when, byte[]? payload = null)
+    public static IsgEvent NewIsgEvent(string id, string stream, string type, string source, ulong position, DateTime when, string payload = "")
     {
         return new IsgEvent()
         {
-            Id = new Uid() { Id = id },
+            Id = new IsgId() { Id = id },
             Stream = System.Enum.Parse<EventStream>(stream),
             Type = System.Enum.Parse<EventType>(type),
             Source = source,
             Position = position,
             When = Timestamp.FromDateTime(when),
-            Payload = payload is null ? ByteString.Empty : ByteString.CopyFrom(payload)
+            Payload = payload
         };
     }
 
@@ -27,11 +29,12 @@ public static class EpoxyHelpers
         var sb = new StringBuilder();
 
         sb.AppendLine(nameof(IsgEvent));
-        sb.AppendLine($"\tId     : {value.Id}");
-        sb.AppendLine($"\tStream : {value.Stream}");
-        sb.AppendLine($"\tType   : {value.Type}");
-        sb.AppendLine($"\tSource : {value.Source}");
-        sb.AppendLine($"\tWhen   : {value.When}");
+        sb.AppendLine($"\tId      : {value.Id}");
+        sb.AppendLine($"\tStream  : {value.Stream}");
+        sb.AppendLine($"\tType    : {value.Type}");
+        sb.AppendLine($"\tSource  : {value.Source}");
+        sb.AppendLine($"\tWhen    : {value.When}");
+        sb.AppendLine($"\tPayload : {value.Payload }");
 
         return sb.ToString();
     }
