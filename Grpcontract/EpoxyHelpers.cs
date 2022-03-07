2a0469f51e4da9c8483724a3dc3e7f2b06da8891
nuchs <sjbrown@live.co.uk>
2022-03-07 11:02:10 +0000

    Events look different depending on whether they are being read or written. Updated contracts to reflect this

---
+++ Grpcontract/EpoxyHelpers.cs
@@ -0,0 +1,38 @@
+﻿using Epoxy.Grpc.Streams;
+using Epoxy.Grpc.Types;
+using Google.Protobuf;
+using Google.Protobuf.WellKnownTypes;
+using System.Text;
+
+namespace Epoxy.Grpc;
+
+public static class EpoxyHelpers
+{
+    public static IsgEvent NewIsgEvent(string id, string stream, string type, string source, ulong position, DateTime when, byte[]? payload = null)
+    {
+        return new IsgEvent()
+        {
+            Id = new Uid() { Id = id },
+            Stream = System.Enum.Parse<EventStream>(stream),
+            Type = System.Enum.Parse<EventType>(type),
+            Source = source,
+            Position = position,
+            When = Timestamp.FromDateTime(when),
+            Payload = payload is null ? ByteString.Empty : ByteString.CopyFrom(payload)
+        };
+    }
+
+    public static string ToPrettyString(this IsgEvent value)
+    {
+        var sb = new StringBuilder();
+
+        sb.AppendLine(nameof(IsgEvent));
+        sb.AppendLine($"\tId     : {value.Id}");
+        sb.AppendLine($"\tStream : {value.Stream}");
+        sb.AppendLine($"\tType   : {value.Type}");
+        sb.AppendLine($"\tSource : {value.Source}");
+        sb.AppendLine($"\tWhen   : {value.When}");
+
+        return sb.ToString();
+    }
+}
